755d14aa2aa831cd789751bf26f54d910e14bdfc
saySthAbout <saysthabout@gmail.com>
2021-10-15 17:56:03 +0900

    프로젝트 생성~회원 도메인 개발

---
+++ core/core/src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 동시성 이슈떄문에 ConcurrentHaspMap 쓴다,
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
