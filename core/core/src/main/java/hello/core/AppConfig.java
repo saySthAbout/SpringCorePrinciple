9c93775d398af46ad2a10b3bd6cc43f0282de92b
saySthAbout <saysthabout@gmail.com>
2021-10-20 15:13:36 +0900

    새로운 구조와 할인 정책 적용
    AppConfig에서 할인정책의 구현을 담당하던 클래스를 FixDiscountPolicy -> RateDiscountPolicy로 바꿨다.

--- core/core/src/main/java/hello/core/AppConfig.java
+++ core/core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -20,11 +21,12 @@ public class AppConfig {
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(memberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }
