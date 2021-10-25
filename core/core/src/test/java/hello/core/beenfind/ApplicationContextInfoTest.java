b293bfab5c5d3d9630e0416614ea6005b7c73c20
saySthAbout <saysthabout@gmail.com>
2021-10-25 16:47:17 +0900

    스프링 빈 조회 - 기본
    올바르게 실행되는 경우의 test코드와
    실패하는 경우의 코드(assertThrows())도 만들어놔야 한다.

--- core/core/src/test/java/hello/core/beenfind/ApplicationContextInfoTest.java
+++ core/core/src/test/java/hello/core/beenfind/ApplicationContextInfoTest.java
@@ -1,8 +1,8 @@
 package hello.core.beenfind;
 
 import hello.core.AppConfig;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
